28fefa6876b0f6b8bf735effd5d857581e7278c8
christophe <christophe.h2001@gmail.com>
2022-07-02 03:50:28 +0200

    Playing music_3

--- main.py
+++ main.py
@@ -65,20 +65,16 @@ async def play(ctx, url : str):
         await ctx.send("Bruh")
 
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith('.mp3'):
-            os.rename(file, 'song_.mp3')
-    voice.play(discord.FFmpegPCMAudio('song_.mp3'))
+    ctx.voice_client.stop()
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format': 'bestaudio'}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+        vc.play(source)
 
 @bot.command()
 async def leave(ctx):
