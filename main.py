1c79861a2986d7886d2d5cef09c03c20046e4415
christophe <christophe.h2001@gmail.com>
2022-07-03 07:21:33 +0200

    répondre à Ben commenté

--- main.py
+++ main.py
@@ -86,11 +86,11 @@ async def resume(ctx):
 async def stop(ctx):
     voice = discord.utils.get(bot.voice_client, guild=ctx.guild)
     voice.stop()
-@bot.event
-async def on_message(message):
-    if (message.author.id == 190155770854244353):
-        channel = message.channel
-        await channel.send("Super cringe wow")
+#@bot.event
+#async def on_message(message):
+ #   if (message.author.id == 190155770854244353):
+  #      channel = message.channel
+   #     await channel.send("Super cringe wow")
 
 bot.run(os.environ["TOKEN"])
 
