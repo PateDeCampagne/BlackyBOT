a64ecd9a47fbaee4c975c76be4195fd93afffab3
christophe <christophe.h2001@gmail.com>
2022-07-06 00:43:07 +0200

    essaie à une améioration pour la lecture de musique

--- main.py
+++ main.py
@@ -58,11 +58,28 @@ async def play(ctx, url : str):
             await ctx.send("Bruh")
 
     ctx.voice_client.stop()
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    YDL_OPTIONS = {'format': 'bestaudio'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn',
+    }
+    YTDL_OPTIONS = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'ytsearch',
+        'source_address': '0.0.0.0',
+    }
     vc = ctx.voice_client
 
-    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+    with youtube_dl.YoutubeDL(YTDL_OPTIONS) as ydl:
         info = ydl.extract_info(url, download=False)
         url2 = info['formats'][0]['url']
         source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
